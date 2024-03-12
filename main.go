0f052de1798da3cf9442cd8d2275d386dd622dbf
fahimfaisaal <fahimfaisal.soikot@gmail.com>
2024-03-12 06:59:32 +0600

    feat: exp done with server

--- main.go
+++ main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"math"
 	"net/http"
 	"os"
+	s "slices"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 
 	concurrent_queue "github.com/fahimfaisaal/exp-go-concurrency/concurrent-queue"
+	chi "github.com/go-chi/chi/v5"
+	middleware "github.com/go-chi/chi/v5/middleware"
 )
 
 type Task struct {
@@ -41,7 +48,7 @@ func ioTask(endpoint string) {
 	fmt.Printf("The status code is: %d\n", res.StatusCode)
 }
 
-func main() {
+func runTasks() {
 	start := time.Now()
 	defer func() {
 		end := time.Now()
@@ -87,4 +94,89 @@ func main() {
 	}()
 
 	wg.Wait()
+}
+
+func runServer() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, world!"))
+	})
+
+	// http://localhost:4000/cpu-tasks/compute?n=10000000000
+	r.Get("/cpu-task/compute", func(w http.ResponseWriter, r *http.Request) {
+		n, err := strconv.ParseUint(r.URL.Query().Get("n"), 10, 64)
+
+		if err != nil {
+			http.Error(w, "Invalid input", http.StatusBadRequest)
+			return
+		}
+
+		task(Task{n, "CPU-Task"})
+
+		w.Write([]byte("Done!"))
+	})
+
+	// http://localhost:4000/cpu-tasks/compute?c=5,numbers=10000000000,10000000000,10000000000,10000000000,10000000000,10000000000,10000000000,10000000000,10000000000,10000000000,10000000000,10000000000,10000000000,10000000000
+	r.Get("/cpu-tasks/compute", func(w http.ResponseWriter, r *http.Request) {
+		n := strings.Split(r.URL.Query().Get("numbers"), ",")
+		c64, err := strconv.ParseUint(r.URL.Query().Get("c"), 10, 32)
+		concurrency := uint(c64)
+
+		if err != nil {
+			concurrency = uint(math.Min(float64(len(n)/2), 30))
+		}
+
+		q := concurrent_queue.New[Task](concurrency, task)
+
+		for index, num := range n {
+			num, err := strconv.ParseUint(num, 10, 64)
+
+			if err != nil {
+				q.Close()
+				http.Error(w, "Invalid input", http.StatusBadRequest)
+				return
+			}
+
+			q.Add(Task{num, fmt.Sprintf("CPU-Task-%d", index+1)})
+		}
+
+		q.Close()
+		w.Write([]byte("Done!"))
+	})
+
+	r.Get("/io-task/wait", func(w http.ResponseWriter, r *http.Request) {
+		delay, err := strconv.ParseUint(r.URL.Query().Get("delay"), 10, 32)
+
+		if err != nil {
+			http.Error(w, "Invalid input", http.StatusBadRequest)
+			return
+		}
+
+		time.Sleep(time.Duration(delay) * time.Second)
+
+		w.Write([]byte("Done!"))
+	})
+
+	logger := log.New(os.Stdout, "[SERVER] ", log.Ldate|log.Ltime)
+
+	server := &http.Server{
+		Addr:     ":4000",
+		Handler:  r,
+		ErrorLog: logger,
+	}
+
+	logger.Printf("Starting server on %s", server.Addr)
+	if err := server.ListenAndServe(); err != nil {
+		logger.Fatalf("Server error: %v", err)
+	}
+}
+
+func main() {
+	if s.Contains(os.Args[1:], "server") {
+		runServer()
+	} else {
+		runTasks()
+	}
 }
